11da069e3fe169ff54551225fa4733e3cb6e3a55
lioliolio <kpc00100@gmail.com>
2017-01-08 15:06:19 +0900

    init staticfiles settings: add explicit STATICFILES_DIRS

--- fastube/fastube/settings/partials/static.py
+++ fastube/fastube/settings/partials/static.py
@@ -1,4 +1,11 @@
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+import os
 
+from .base import BASE_DIR
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "fastube", "static"),
+]
 STATIC_URL = '/static/'
