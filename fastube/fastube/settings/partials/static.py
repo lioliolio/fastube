c97abf23446449b8bdf948d34b013193123dbce5
lioliolio <kpc00100@gmail.com>
2017-01-09 18:33:04 +0900

    init django-pipeline: manage staticfiles

--- fastube/fastube/settings/partials/static.py
+++ fastube/fastube/settings/partials/static.py
@@ -11,3 +11,23 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT_DIR, "dist", "static")
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
+    'pipeline.finders.PipelineFinder',
+)
+
+PIPELINE = {
+    'STYLESHEETS': {
+        'fastube': {
+            'source_filenames': (
+              'css/application.css',
+              'css/partials/*.css',
+            ),
+            'output_filename': 'css/fastube.css',
+        }
+    }
+}
