9368570b16d4a5e9ab4f07f203a7fb1dcb972786
lioliolio <kpc00100@gmail.com>
2017-01-10 16:31:27 +0900

    init LoginView

---
+++ fastube/users/views/login.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+
+class LoginView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "users/login.html",
+            context={},
+        )
