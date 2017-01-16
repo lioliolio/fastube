6489ba3701e2c4cf585d3eee89f81a28a53446a0
lioliolio <kpc00100@gmail.com>
2017-01-16 20:13:21 +0900

    update PostCommentListCreateAPIView: create comment on ajax request

--- fastube/fastube/static/js/posts.js
+++ fastube/fastube/static/js/posts.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
-    var commentsSectionElement = $("#comments-section");
+	// CSRF Token settings
+	// https://docs.djangoproject.com/en/1.9/ref/csrf/
+
+	// using jQuery
+	function getCookie(name) {
+		var cookieValue = null;
+		if (document.cookie && document.cookie !== '') {
+			var cookies = document.cookie.split(';');
+			for (var i = 0; i < cookies.length; i++) {
+				var cookie = jQuery.trim(cookies[i]);
+				// Does this cookie string begin with the name we want?
+				if (cookie.substring(0, name.length + 1) === (name + '=')) {
+					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+					break;
+				}
+			}
+		}
+		return cookieValue;
+	}
+	var csrftoken = getCookie('csrftoken');
+	
+	function csrfSafeMethod(method) {
+		// these HTTP methods do not require CSRF protection
+		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	}
+	$.ajaxSetup({
+		beforeSend: function(xhr, settings) {
+			if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		}
+	});
+    
+	var commentsSectionElement = $("#comments-section");
 
     var commentsCountElement = $(commentsSectionElement).find("#comments-count");
 
@@ -28,13 +61,39 @@ $(document).ready(function() {
                 $(commentsListElement).append(commentListElement);
             });
         },
-        error: function(data) {
+        error: function(error) {
         }
     });
     commentsCreateFormElement.submit(function() {
         var content = $(commentsCreateFormInputContentElement).val();
-        alert(content);
-        $(commentsCreateFormInputContentElement).val("");
+		var data = {
+			content: content
+		};
+
+        $.ajax({
+            type: "POST",
+            url: commentsAPIUrl,
+            data: data,
+            success: function(data){
+                var commentUsername = data.username;
+                var commentContent = data.content;
+
+                // Append Comments List
+                var commentData = commentUsername + ": " + commentContent;
+                var commentListElement = $("<li>").text(commentData);
+                $(commentsListElement).append(commentListElement);
+
+                // Update Comments Count
+                var commentsCount = $(commentsCountElement).html();
+                var newCommentsCount = Number(commentsCount) + 1;
+                $(commentsCountElement).html(String(newCommentsCount));
+
+                $(commentsCreateFormInputContentElement).val("");
+            },
+            error: function(error){
+                console.log(error);
+            },
+        });
 
         return false;
     });
