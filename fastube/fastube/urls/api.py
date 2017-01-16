55201fd50199490af1ff705fa269c211ebe8850d
lioliolio <kpc00100@gmail.com>
2017-01-16 15:30:47 +0900

    init PostCommentListAPIView

--- fastube/fastube/urls/api.py
+++ fastube/fastube/urls/api.py
@@ -4,7 +4,8 @@ from posts.api import *
 
 
 urlpatterns = [
-    url(r'^posts/$', include([
+    url(r'^posts/', include([
         url(r'^$', PostListAPIView.as_view(), name="list"),
+        url(r'^(?P<slug>\w+)/comments/$', PostCommentListAPIView.as_view(), name="comments"),
     ], namespace="posts")),
 ]
