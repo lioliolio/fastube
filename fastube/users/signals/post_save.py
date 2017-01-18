1bfde135837a013aeeda51df920191e97869c19c
lioliolio <kpc00100@gmail.com>
2017-01-18 19:12:26 +0900

    update post_save_user: implement send_slack_message functionality

--- fastube/users/signals/post_save.py
+++ fastube/users/signals/post_save.py
@@ -1,10 +1,25 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+import os
+import json
+import requests
+from time import sleep
 
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 @receiver(post_save, sender=get_user_model())
 def post_save_user(sender, instance, created, **kwargs):
     if created:
-        pass
+        sleep(3)
+        slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+        data = {
+            "text": "{username} 님이 회원가입 했습니다.".format(username=instance.username),
+            "username": "fastube",
+            "icon_emoji": ":ghost:",
+        }
+
+        response = requests.post(
+            slack_webhook_url,
+            data=json.dumps(data),
+        )
