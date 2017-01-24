223136138522ec04fa18f893f62b455b96a3bd55
lioliolio <kpc00100@gmail.com>
2017-01-24 18:54:28 +0900

    update settings: change database url to environment variable DATABASE_URL

--- fastube/fastube/settings/partials/database.py
+++ fastube/fastube/settings/partials/database.py
@@ -2,12 +2,19 @@
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 import os
 
+import dj_database_url
+
 from .base import BASE_DIR
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(
+    default=os.environ.get("DATABASE_URL"),
+)
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
